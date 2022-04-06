91fbaae9cbfce61a9356fb4527176e236b523d9e
Erwann <erwannmillon@gmail.com>
2022-04-06 18:28:39 +0200

    a

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 23:06:15 by gmillon           #+#    #+#             */
-/*   Updated: 2022/04/05 23:35:03 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/04/06 18:28:25 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,54 +22,44 @@ void	message_received(int x)
 	ft_putstr_fd("Message sent successfully!", 1);
 }
 
-int transmit_char(int c, int serverpid)
+int	transmit_char(int c, int serverpid)
 {
-	int	i;
-	int a = 0;
-	i = 7;
+	int					i;
+	struct sigaction	newaction;
 
-	struct sigaction newaction;
-	
+	i = 7;
 	newaction.sa_handler = &receive_str;
 	sigaction(SIGUSR1, &newaction, NULL);
 	signal(SIGUSR2, &message_received);
-	while (i >=0)
+	while (i >= 0)
 	{
 		if (check_bit(&c, i))
 		{
-			printf("%d", check_bit(&c, i));
 			kill(serverpid, SIGUSR1);
-			// set_bit(&a, i);
 		}
 		else
 		{
-			printf("%d", check_bit(&c, i));
 			kill(serverpid, SIGUSR2);
 		}
 		i--;
 		pause();
 	}
-	// printf("\n");
 	return (1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	char	*transmitstr;
-	int		i;
-	int		serverpid;
-
-	
-	struct sigaction newaction;
+	char				*transmitstr;
+	int					i;
+	int					serverpid;
+	struct sigaction	newaction;
 
 	newaction.sa_handler = &receive_str;
 	sigaction(SIGUSR1, &newaction, NULL);
 	signal(SIGUSR2, &message_received);
-
-
 	i = 0;
 	if (argc != 3)
-		return(printf("Invalid number of arguments"));
+		return (write(1, "Invalid number of arguments", 27));
 	transmitstr = argv[1];
 	serverpid = atoi(argv[2]);
 	while (transmitstr[i])
--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 23:05:53 by gmillon           #+#    #+#             */
-/*   Updated: 2022/04/06 09:32:38 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/04/06 18:24:54 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,23 +38,14 @@ void receive_char(int signum, siginfo_t *info, void *ucontext)
 	static char	*str = NULL;
 	if (!str)
 		str = malloc(1000);
-	// write(1, "Signal number\n", 15);
-	// ft_putnbr_fd(info->si_signo, 1);
 	if (signum == SIGUSR1)
 	{
 		set_bit(&c, i);
-		// write(1, "1", 1);
 	}
 	if (signum == SIGUSR2)
 	{
 		// write(1, "0", 1);
 	}
-	// ft_putstr_fd("i:   ", 1);
-	// ft_putnbr_fd(i, 1);
-	// ft_putstr_fd("counter:   ", 1);
-
-	// ft_putstr_fd("\n", 1);
-	
 	i--;
 	if (c == 255)
 	{
@@ -62,23 +53,15 @@ void receive_char(int signum, siginfo_t *info, void *ucontext)
 		c = 0;
 		kill(info->si_pid, SIGUSR2);
 		str[counter] = 0;
-		// ft_putstr_fd("sicces", 1);
-		// printf("%s coutner: %d", str, counter);
 		write(1, str, counter);
 		counter = 0;
-
 		// free(str);
 		// str = NULL;
 	}
 	if (i == -1 && c != 255)
 	{
-		// ft_putchar_fd(c, 1);
-		// ft_putstr_fd("sicces", 1);
-		// ft_putstr_fd("counter:     ", 1);
-		// ft_putnbr_fd(counter, 1);
 		str[counter] = c;
 		counter++;
-
 		if (c!= 0 && c % 800 == 0)
 			str = buf_alloc(str);
 		i = 7;
@@ -86,11 +69,8 @@ void receive_char(int signum, siginfo_t *info, void *ucontext)
 		usleep(1000);
 		kill(info->si_pid, SIGUSR1);
 	}
-	// if (counter >= 2)
-	// 	printf("STRING: %s\n", g_str);
 	usleep(1000);
 	kill(info->si_pid, SIGUSR1);
-
 }
 
 
