d8597b81ee43d6e2d6144ba723da78cc5a19db6b
Erwann <erwannmillon@gmail.com>
2022-04-06 18:33:13 +0200

    norm

--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 23:05:53 by gmillon           #+#    #+#             */
-/*   Updated: 2022/04/06 18:24:54 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/04/06 18:32:43 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*buf_alloc(char *old)
 {
 	char		*new;
 	static int	size = 1005;
-	
+
 	if (!old)
 	{
 		size = 1005;
@@ -27,24 +27,21 @@ char	*buf_alloc(char *old)
 	new = malloc(size);
 	ft_memcpy(new, old, size - 4);
 	free(old);
-	return(new);
+	return (new);
 }
 
-void receive_char(int signum, siginfo_t *info, void *ucontext)
+void	receive_char(int signum, siginfo_t *info, void *ucontext)
 {
 	static int	counter = 0;
 	static int	i = 7;
 	static int	c = 0;
 	static char	*str = NULL;
+
 	if (!str)
 		str = malloc(1000);
 	if (signum == SIGUSR1)
 	{
 		set_bit(&c, i);
-	}
-	if (signum == SIGUSR2)
-	{
-		// write(1, "0", 1);
 	}
 	i--;
 	if (c == 255)
@@ -55,14 +52,14 @@ void receive_char(int signum, siginfo_t *info, void *ucontext)
 		str[counter] = 0;
 		write(1, str, counter);
 		counter = 0;
-		// free(str);
-		// str = NULL;
+		free(str);
+		str = NULL;
 	}
 	if (i == -1 && c != 255)
 	{
 		str[counter] = c;
 		counter++;
-		if (c!= 0 && c % 800 == 0)
+		if (c != 0 && c % 800 == 0)
 			str = buf_alloc(str);
 		i = 7;
 		c = 0;
@@ -73,21 +70,18 @@ void receive_char(int signum, siginfo_t *info, void *ucontext)
 	kill(info->si_pid, SIGUSR1);
 }
 
-
-int main(void)
+int	main(void)
 {
-	int i = 0;
-	sigset_t	set;
-	struct sigaction newaction;
+	int						i;
+	struct sigaction		newaction;
+	long long unsigned int	pid;
+
+	i = 0;
 	newaction.sa_flags = SA_SIGINFO;
 	newaction.sa_sigaction = &receive_char;
-
-	sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigaddset(&set, SIGUSR2);
 	sigaction(SIGUSR1, &newaction, NULL);
 	sigaction(SIGUSR2, &newaction, NULL);
-	long long unsigned int pid = getpid();
+	pid = getpid();
 	ft_putnbr_fd(pid, 1);
 	printf("\n");
 	while (1)
